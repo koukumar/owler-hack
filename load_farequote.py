1e1958fea8b3362ee572c26a8950cc831025c727
koukumar <kousik.kumar@gmail.com>
2014-12-20 22:11:40 +0530

    bug fix

--- load_farequote.py
+++ load_farequote.py
@@ -8,8 +8,7 @@ def main(host='localhost', port=8086):
     user = 'root'
     password = 'root'
     dbname = 'Airfare'
-    airline_data = load();
-    json_body = get_data()
+    json_body = get_airline_data()
     client = InfluxDBClient(host, port, user, password, dbname)
 
     client.delete_database(dbname)
@@ -19,7 +18,7 @@ def main(host='localhost', port=8086):
     print("Writing data")
     client.write_points(json_body)
 
-def load():
+def load_airline_date():
   with open('farequote.csv', 'rb') as csvfile:
     farequotes = csv.reader(csvfile, delimiter=',')
     list1 = []
@@ -32,20 +31,13 @@ def load():
         list1.append(row)
     return list1
 
-def get_data():
-    airline_data = load();
+def get_airline_data():
+    airline_data = load_airline_date();
     fields = airline_data[0];
     json_body = [{
         "points": airline_data[1 : len(airline_data)],
         "name": "metrics",
         "columns": fields
-    }]
-    json1_body = [{
-        "points": [
-	   ["2014-06-27 17:39:52Z", 20]
-        ],
-        "name": "foo",
-        "columns": ["time", "column_two"]
     }]
     return json_body
 
